b2e1f2442564d140b70f75dce2f8ffa3e660d58b
sanunavwoo <sanunavodey@gmail.com>
2022-09-03 00:53:33 +0530

    fix- zero validation
    Added error message for 0 as input

--- hypotenuse.js
+++ hypotenuse.js
@@ -25,9 +25,9 @@ calculate.addEventListener("click", ()=>{
         outputArea.innerHTML="Height or base can't be negative. Eliminate negativity";
     }
     else if(baseVal==0 || heightVal==0){
-        console.log("Sides of a triangle cannot be 0 practically");
+        console.log("Base and Height of a triangle cannot be 0 practically");
         outputArea.style.color="red";
-        outputArea.innerHTML="Sides of a triangle cannot be 0 practically.\nPlease enter values greater than 0";
+        outputArea.innerHTML="Base and Height of a triangle cannot practically be 0.\nPlease enter values greater than 0";
     }
     else{
         outputArea.style.color="rgb(106, 201, 69)";
